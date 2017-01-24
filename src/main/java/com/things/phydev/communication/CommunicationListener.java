c80c393266428f19c31368ec22cd41f6ab5731db
fortran <qnev89@gmail.com>
2017-01-24 21:54:48 +0200

    edit contact information

--- src/main/java/com/things/phydev/communication/CommunicationListener.java
+++ src/main/java/com/things/phydev/communication/CommunicationListener.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c)  2016 Ivelin Yanev <qnev89@gmail.com>.
+ * Copyright (c)  2016 Ivelin Yanev <bgfortran@gmail.com>.
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
