d729c30fb5265e78e6a2710c9e494fab5c72c957
fortran <qnev89@gmail.com>
2017-03-20 23:10:32 +0200

    added some additional methods

--- src/main/java/com/things/phydev/impl/comunication/DeviceAccessImpl.java
+++ src/main/java/com/things/phydev/impl/comunication/DeviceAccessImpl.java
@@ -18,14 +18,48 @@ import com.things.phydev.communication.CommunicationListener;
 import com.things.phydev.communication.DeviceAccess;
 import com.things.phydev.communication.Packet;
 import com.things.phydev.communication.ReceivedMessage;
+import com.things.phydev.communication.TransportListener;
+import com.things.phydev.transport.Transport;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DeviceAccessImpl implements DeviceAccess {
+public class DeviceAccessImpl implements DeviceAccess, TransportListener{
+	
+	private static DeviceAccessImpl instance;
+	private Transport transportStream;
+	private List<CommunicationListener> communicationListeners;
+	
+	/**
+	 * 
+	 * @param msTransport
+	 */
+	private DeviceAccessImpl(Transport mTransport) {
+		this.communicationListeners = new ArrayList<CommunicationListener>();
 
+		this.transportStream = mTransport;
+	}
+	
+	/**
+	 * Returns the Singleton instance if <code>DeviceAccessImpl</code> class,
+	 * creating it if necessary.
+	 * @param msTransport
+	 * @return
+	 */
+	public static DeviceAccessImpl getInstance(Transport msTransport) {
+		if (instance == null) {
+			instance = new DeviceAccessImpl(msTransport);
+		}
+
+		return instance;
+	}
+	
     @Override
     public void postMessage(Packet pkData) throws IOException {
-
+        pkData.getDataFormat();
+		
+		this.transportStream.sendData(pkData);
     }
 
     @Override
@@ -35,11 +69,28 @@ public class DeviceAccessImpl implements DeviceAccess {
 
     @Override
     public void addCommunicationListener(CommunicationListener communicationListener) {
-
+    	if (communicationListener != null) {
+			this.communicationListeners.add(communicationListener);
+		}
     }
 
     @Override
     public void removeCommunicationListener(CommunicationListener communicationListener) {
-
+    	if (communicationListener != null) {
+			this.communicationListeners.remove(communicationListener);
+		}
     }
+
+	@Override
+	public void transportDataRecived(ReceivedMessage btMessage) {
+		if (btMessage != null) {
+			notifyListeners(btMessage);
+		}		
+	}
+	
+	public void notifyListeners(ReceivedMessage btMessage) {
+		for (CommunicationListener communicationListener : communicationListeners) {
+			communicationListener.messageRecieved(btMessage);
+		}
+	}
 }
